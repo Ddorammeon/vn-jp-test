c92efce8375debdb66da9de5aa6861ea1ccdf4e2
Ddorammeon <kirikarakazuto85@gmail.com>
2024-05-15 12:03:40 +0700

    nah

---
+++ connection.php
@@ -0,0 +1,39 @@
+<?php
+
+
+	require './PHPMailer-master/src/Exception.php';
+	require './PHPMailer-master/src/PHPMailer.php';
+	require './PHPMailer-master/src/SMTP.php';
+    use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+
+	$name = $_POST["name"];
+	$phone = $_POST["phone"];
+
+	$mail = new PHPMailer(true);
+
+	$mail->SMTPDebug = 2;
+	$mail ->isSMTP();
+	$mail ->SMTPAuth = "true";
+
+	$mail ->Host = "smtp.gmail.com";
+	$mail ->SMTPSecure = 'ssl';
+	$mail ->Port = 465;
+
+	$mail ->Username = "dmon70998@gmail.com";
+	$mail ->Password = "mon111111";
+
+	$mail ->setFrom("dmon70998@gmail.com", "mon111111");
+	$mail ->addAddress("dmon70998@gmail.com", "mon111111");
+
+
+	$mail->isHTML(true);
+	$message = "Name: $name\nEmail: $phone";
+	$subject = "New Submission";
+	$mail ->send();
+	
+	echo "Email sent successfully!";
+	
+
+?>
